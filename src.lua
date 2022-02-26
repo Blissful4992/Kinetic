f556d1d1f53c589cd441d2dfdc2ec39a63cfc8ac
Blissful <53982126+Blissful4992@users.noreply.github.com>
2022-02-27 00:40:36 +0100

    Update src.lua

--- src.lua
+++ src.lua
@@ -723,23 +723,7 @@ Library.NewWindow = function(window_info)
         local Container = NEW("Frame")
         local NotificationLayout = NEW("UIListLayout")
 
-        Notifications_Folder.Name = "Notifications"
-        Notifications_Folder.Parent = UI
-
-        Container.Name = "Container"
-        Container.Parent = Notifications_Folder
-        Container.BackgroundColor3 = RGB(255, 255, 255)
-        Container.BackgroundTransparency = 1.000
-        Container.BorderSizePixel = 0
-        Container.Position = U2(1, -210, 0, -10)
-        Container.Size = U2(0, 200, 1, 0)
-        Container.ZIndex = 10000
-
-        NotificationLayout.Name = "NotificationLayout"
-        NotificationLayout.Parent = Container
-        NotificationLayout.SortOrder = Enum.SortOrder.LayoutOrder
-        NotificationLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-        NotificationLayout.Padding = UDim.new(0, 5)
+        Notifications_Folder.Name="Notifications"Notifications_Folder.Parent=UI;Container.Name="Container"Container.Parent=Notifications_Folder;Container.BackgroundColor3=RGB(255,255,255)Container.BackgroundTransparency=1.000;Container.BorderSizePixel=0;Container.Position=U2(1,-210,0,-10)Container.Size=U2(0,200,1,0)Container.ZIndex=10000;NotificationLayout.Name="NotificationLayout"NotificationLayout.Parent=Container;NotificationLayout.SortOrder=Enum.SortOrder.LayoutOrder;NotificationLayout.VerticalAlignment=Enum.VerticalAlignment.Bottom;NotificationLayout.Padding=UDim.new(0,5)
 
         function root.NewNotification(notification_info)
             notification_info.Title = notification_info.Title or "Notification"
@@ -752,56 +736,7 @@ Library.NewWindow = function(window_info)
             local Body = NEW("TextLabel")
             local Progress = NEW("Frame")
 
-            Notification.Name = "Notification"
-            Notification.Parent = Container
-            Notification.BackgroundColor3 = WinTheme.Background
-            Notification.BorderColor3 = WinTheme.Dark_Borders
-            Notification.ClipsDescendants = true
-            Notification.Size = U2(1, 0, 0, 0)
-            Notification.ZIndex = 10001
-
-            Notif_Title.Name = "Notif_Title"
-            Notif_Title.Parent = Notification
-            Notif_Title.BackgroundTransparency = 1.000
-            Notif_Title.Position = U2(0, 5, 0, 0)
-            Notif_Title.Size = U2(1, -5, 0, 20)
-            Notif_Title.ZIndex = 10002
-            Notif_Title.Font = Enum.Font.GothamSemibold
-            Notif_Title.Text = notification_info.Title
-            Notif_Title.TextColor3 = WinTheme.Text_Color
-            Notif_Title.TextSize = 12.000
-            Notif_Title.TextXAlignment = Enum.TextXAlignment.Left
-
-            Border.Name = "Border"
-            Border.Parent = Notification
-            Border.BackgroundColor3 = WinTheme.Dark_Borders
-            Border.BorderSizePixel = 0
-            Border.Position = U2(0, 0, 0, 20)
-            Border.Size = U2(1, 0, 0, 1)
-            Border.ZIndex = 10002
-
-            Body.Name = "Body"
-            Body.Parent = Notification
-            Body.BackgroundColor3 = RGB(255, 255, 255)
-            Body.BackgroundTransparency = 1.000
-            Body.Position = U2(0, 5, 0, 25)
-            Body.Size = U2(1, -10, 1, -30)
-            Body.ZIndex = 10002
-            Body.Font = GTHM
-            Body.Text = notification_info.Body
-            Body.TextWrapped = true
-            Body.TextColor3 = RGB(255, 255, 255)
-            Body.TextSize = 12.000
-            Body.TextXAlignment = Enum.TextXAlignment.Left
-            Body.TextYAlignment = Enum.TextYAlignment.Top
-
-            Progress.Name = "Progress"
-            Progress.Parent = Notification
-            Progress.BackgroundColor3 = WinTheme.Accent
-            Progress.BorderSizePixel = 0
-            Progress.Position = U2(0, 0, 1, -1)
-            Progress.Size = U2(0, 0, 0, 1)
-            Progress.ZIndex = 10002
+            Notification.Name="Notification"Notification.Parent=Container;Notification.BackgroundColor3=WinTheme.Background;Notification.BorderColor3=WinTheme.Dark_Borders;Notification.ClipsDescendants=true;Notification.Size=U2(1,0,0,0)Notification.ZIndex=10001;Notif_Title.Name="Notif_Title"Notif_Title.Parent=Notification;Notif_Title.BackgroundTransparency=1.000;Notif_Title.Position=U2(0,5,0,0)Notif_Title.Size=U2(1,-5,0,20)Notif_Title.ZIndex=10002;Notif_Title.Font=Enum.Font.GothamSemibold;Notif_Title.Text=notification_info.Title;Notif_Title.TextColor3=WinTheme.Text_Color;Notif_Title.TextSize=12.000;Notif_Title.TextXAlignment=Enum.TextXAlignment.Left;Border.Name="Border"Border.Parent=Notification;Border.BackgroundColor3=WinTheme.Dark_Borders;Border.BorderSizePixel=0;Border.Position=U2(0,0,0,20)Border.Size=U2(1,0,0,1)Border.ZIndex=10002;Body.Name="Body"Body.Parent=Notification;Body.BackgroundColor3=RGB(255,255,255)Body.BackgroundTransparency=1.000;Body.Position=U2(0,5,0,25)Body.Size=U2(1,-10,1,-30)Body.ZIndex=10002;Body.Font=GTHM;Body.Text=notification_info.Body;Body.TextWrapped=true;Body.TextColor3=RGB(255,255,255)Body.TextSize=12.000;Body.TextXAlignment=Enum.TextXAlignment.Left;Body.TextYAlignment=Enum.TextYAlignment.Top;Progress.Name="Progress"Progress.Parent=Notification;Progress.BackgroundColor3=WinTheme.Accent;Progress.BorderSizePixel=0;Progress.Position=U2(0,0,1,-1)Progress.Size=U2(0,0,0,1)Progress.ZIndex=10002
 
             --
             WRAP(function()
