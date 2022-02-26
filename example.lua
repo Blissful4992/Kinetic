0489dfbdc4c305d4bf393614190a1e4e9c51e46a
Blissful <53982126+Blissful4992@users.noreply.github.com>
2022-02-26 16:53:28 +0100

    Update example.lua

--- example.lua
+++ example.lua
@@ -15,18 +15,26 @@ local Overrides = {
 
 local Window = Library.NewWindow({
     Text = "Test UI",
-    WindowSize = {X = 550, Y = 450},
+
+    WindowSize = Vector2.new(550, 450),     -- Initial Size of the Window
+    WindowPosition = Vector2.new(400, 200), -- Initial Position of the Window
 
     ThemeOverrides = Overrides,
     
-    -- WindowSizeCallBack will fire everytime the user changes the size of the UI, 
+    -- WindowSizeCallback will fire everytime the user changes the size of the UI, 
     -- you can use this to save the size into a config system for example
-    WindowSizeCallBack = function(new)
+    WindowSizeCallback = function(new)
         print("You changed the window size to: " .. new.X .. ", " .. new.Y)
     end,
 
-    -- CloseCallBack will fire when the user presses the close button on the UI (Top Right)
-    CloseCallBack = function()
+    -- WindowPositionCallback will fire everytime the user moves/drags the UI to a new position, 
+    -- you can use this to save the position into a config system for example
+    WindowPositionCallback = function(new)
+        print("You moved the window to: " .. new.X .. ", " .. new.Y)
+    end,
+
+    -- CloseCallback will fire when the user presses the close button on the UI (Top Right)
+    CloseCallback = function()
         print("You closed the script !")
     end
 })
@@ -37,39 +45,53 @@ local Section = Page.NewSection({Text = "Section 1"})
 
 local Button = Section.NewButton({
     Text = "Click Me", 
-    CallBack = function()
+    Callback = function()
         print("I got clicked !")
     end, 
     Description = "This can be used as a one time feature !"
 })
 
 local Toggle = Section.NewToggle({
-    Text = "Toggle Me", 
-    CallBack = function(bool)
+    Text = "Toggle Me",
+    Callback = function(bool)
         print(bool)
     end, 
     Default = true,
     Description = "This turns ON and OFF when you click it !"
 })
 
+local TextBox = Section.NewTextBox({
+    Text = "Type", 
+    PlaceHolderText = "Type !",
+    Callback = function(text)
+        print("New Input: " .. text)
+    end, 
+
+    Default = "Green",
+    Description = "Type anything in this box !",
+
+    OnlyNumeric = true,         -- Only Allow Numeric Output (Numbers)
+    -- OnlyAlphabetic = true    -- Only Allow Alphabetic Output (Alphabet)
+})
+
 local Slider = Section.NewSlider({
     Text = "Slide Me",
-    CallBack = function(value)
+    Callback = function(value)
         print("You slid to: " .. value)
     end,
     Default = 50,
     Min = 1, Max = 100,
-    Decimals = 0, -- Number of decimal numbers after the .
+    Decimals = 0, -- Number of decimal numbers to show after the period/comma
     Suffix = "m",
     Description = "You can slide me with your mouse to change the value !"
 })
 
 local Dropdown = Section.NewDropdown({
     Text = "Select Me", 
-    CallBack = function(option)
+    Callback = function(option)
         print("You selected: " .. option)
     end, 
-    Options = {
+    Options = { -- Spoiler : Won't be in order
         "Red",
         "Green",
         "Blue"
@@ -80,10 +102,10 @@ local Dropdown = Section.NewDropdown({
 
 local Keybind = Section.NewKeybind({
     Text = "Press Me", 
-    CallBack = function()
+    Callback = function()
         print("You pressed the right key !")
     end, 
-    KCallBack = function(new)
+    KeyCallback = function(new)
         print("You changed the key to: " .. string.sub(tostring(new), 14, #tostring(new)))
     end, 
     Default = Enum.KeyCode.X,
@@ -92,7 +114,7 @@ local Keybind = Section.NewKeybind({
 
 local Picker = Section.NewColorPicker({
     Text = "Pick Me", 
-    CallBack = function(color)
+    Callback = function(color)
         print("You picked the new color: " .. color.R .. ", " .. color.G .. ", " .. color.B)
     end, 
     Default = Color3.fromRGB(0, 50, 0),
@@ -101,9 +123,9 @@ local Picker = Section.NewColorPicker({
 
 local Chipset = Section.NewChipset({
     Text = "Select Me", 
-    CallBack = function(tbl)
+    Callback = function(tbl)
         print("New Options:")
-        for i,v in pairs(tbl) do
+        for i,v in next, tbl do
             print("    " .. tostring(i) .. ": " .. tostring(v))
         end
     end, 
@@ -118,7 +140,7 @@ local Chipset = Section.NewChipset({
 local PlayerTbl = {}
 local PlayerList = Section.NewPlayerChipset({
     Text = "Player List", 
-    CallBack = function(tbl) 
+    Callback = function(tbl) 
         PlayerTbl = tbl
     end, 
     Description = "Select individual players from this auto-updating list of players !"
